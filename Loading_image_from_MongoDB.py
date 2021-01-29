3526574864e418aafffdb8a08908b6ffcc840b6e
bubufun <bitterbu@gmail.com>
2021-01-29 20:56:24 +0800

    test version

---
+++ Loading_image_from_MongoDB.py
@@ -0,0 +1,20 @@
+import pymongo
+from gridfs import *
+
+def loading_image():
+    myclientdata = pymongo.MongoClient("mongodb+srv://peter:0987602620@cluster0.0qqo9.mongodb.net/ceb101?retryWrites=true&w=majority")
+    mydbdata = myclientdata['test']
+    # mycoldata = mydbdata['trash']
+
+    
+    gridFS = GridFS(mydbdata, collection="fs")
+    count=0
+    for grid_out in gridFS.find():
+        count+=1
+        print(count)
+        data = grid_out.read() # 獲取圖片資料
+        outf = open('%s.jpg'% count,'wb')#建立檔案
+        outf.write(data)  # 儲存圖片
+        outf.close()
+if __name__ == '__main__':
+    loading_image()
